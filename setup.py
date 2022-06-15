c0b69321c6e03b5b1ccdd9fedbf10cc45aa295af
user <akshat.thakur8@gmail.com>
2022-06-15 04:34:51 +0000

    commit message

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project is to analyze fitness trends of 2022 and using Data Science to improve general health and fitness.',
+    author='Akshat Thakur',
+    license='',
+)
